06ae7ca989b68637d2a5ecaece1672abb00b549b
“Naty-S” <na7a5cha010@gmail.com>
2022-07-21 18:31:38 -0400

    + more aliases & paths

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ const config = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
+			$assets: path.resolve('./src/assets'),
 			$components: path.resolve('./src/components'),
 			$types: path.resolve('./src/types'),
 			$utils: path.resolve('./src/utils'),
+			$stores: path.resolve('./src/stores'),
 		},
 	}
 };
