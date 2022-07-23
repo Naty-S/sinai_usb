21d20eaecf046c3c53c347bb9b0fa1959a3c1b69
“Naty-S” <na7a5cha010@gmail.com>
2022-07-22 22:10:28 -0400

    + base menu

---
+++ src/components/menu.svelte
@@ -0,0 +1,63 @@
+<div class="uk-navbar-container" uk-navbar>
+  <div class="uk-navbar-center">
+    <a href="/" class="uk-navbar-item uk-logo">DI</a>
+    <ul class="uk-navbar-nav">
+      <li>
+        item 1
+      </li>
+    </ul>
+    <div class="uk-navbar-item">item 2</div>
+  </div>
+  <!-- <div class="uk-navbar-right">
+    <ul class="uk-navbar-nav">
+      <li>
+        <a href="">Parent</a>
+        <div class="uk-navbar-dropdown">
+          <ul class="uk-nav uk-navbar-dropdown-nav">
+            
+            <li class="uk-nav-header uk-inline">
+              <button class="uk-button uk-button-primary" type="button">Actividades</button>
+              <div class="uk-flex-inline" uk-dropdown="mode: click">
+                <li><a href="#">Ingresar Nueva Actividad</a></li>
+                <li><a href="#">Revisar Mis Actividades</a></li>
+              </div>
+            </li>
+            
+            <li class="uk-nav-divider"></li>
+
+            <li class="uk-nav-header uk-inline">
+              <button class="uk-button uk-button-primary" type="button">Vistas</button>
+              <div class="uk-inline" uk-dropdown="mode: click">
+                <li><a href="#">Certificación PEII</a></li>
+                <li><a href="#">Constancias Grupos</a></li>
+                <li><a href="#">Constancias Proyectos</a></li>
+                <li><a href="#">Evaluación GID</a></li>
+                <li><a href="#">Evaluación S2</a></li>
+                <li><a href="#">Evaluación S2 prueba</a></li>
+                <li><a href="#">Vista BRA</a></li>
+                <li><a href="#">Vista Coordinación</a></li>
+                <li><a href="#">Vista Departamento</a></li>
+                <li><a href="#">Vista Grupos</a></li>
+                <li><a href="#">GID- ?></a></li>
+                <li><a href="#">GID-</a></li>
+                <li><a href="#">Vista Personal</a></li>
+                <li><a href="#">Vista Referencias Puras</a></li>
+              </div>
+            </li>
+            
+            <li class="uk-nav-divider"></li>
+
+            <li class="uk-nav-header uk-inline">
+              <button class="uk-button uk-button-primary" type="button">Opciones</button>
+              <div class="uk-inline" uk-dropdown="mode: click">
+                <li><a href="#">Cambiar Contraseña</a></li>
+                <li><a href="#">Cambiar Perfil</a></li>
+              </div>
+            </li>
+            
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div> -->
+</div>
