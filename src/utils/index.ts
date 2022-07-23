2e5da9c633024c65848304eb99db64735821f9e7
“Naty-S” <na7a5cha010@gmail.com>
2022-07-23 16:07:49 -0400

    acts titles complete

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,12 @@
+import type { ActivityKind } from "$types/db/actividades";
+
 /**
  * Format the date to 'Month year'
  * 
  * @param {Date | string} date - The date to format
  * @returns {string} The formated date in 'Month year'
  */
-export const format_date: (date: Date | string) => string = (date: Date | string) => {
+export const format_date = (date: Date | string): string => {
   const _date = typeof date === "string" ? new Date(date) : date;
   const month = _date.toLocaleDateString('es', { month: "long" });
   const year = _date.getFullYear();
@@ -17,37 +19,39 @@ export const format_date: (date: Date | string) => string = (date: Date | string
  * for the display of the activities by year
  * 
  * @param {string} kind - The kind of the activity
+ * @param {ActivityKind} act_kind - Activity kind info
  * @returns {string} The display title
  */
-export const match_kind_to_title: (kind: string) => string = (kind: string) => {
+export const match_kind_to_title = (kind: string, act_kind: ActivityKind): string => {
+
+  // 'act_info' can't be 'undefined' because its verified in the parent
 
   switch (kind) {
     case "articulos_revistas":
-      /* switch (key) {
-        case value:
-          // indice=SCI or SSCI or SCIENCE CITATION INDEX or SOCIAL SCIENCES CITATION INDEX
-          return "Publicaciones en Revistas Indexadas en el SCI-SSCI-ARTS"
+    
+      if ( ["SCI", "SSCI", "SCIENCE CITATION INDEX", "SOCIAL SCIENCES CITATION INDEX"].
+            some(i => act_kind.indice.toUpperCase().includes(i)) ) {
+        return "Publicaciones en Revistas Indexadas en el SCI-SSCI-ARTS"
+    
+      } else if (act_kind.indice && act_kind.estado === "Publicado") {
+        return "Publicaciones en Revistas Indexadas en Otros Indices"
       
-        case value:
-          // indice is not null & estado=publicado & indice!=todos los anteriores
-          return "Publicaciones en Revistas Indexadas en Otros Indices Internacional"
-      
-        case value:
-          // indice is not null & estado=publicado & indice!=todos los anteriores & otra cosa
-          return "Publicaciones en Revistas Indexadas en Otros Indices"
-      
-        case value:
-          // indice is not null & estado=publicado
-          return "Publicaciones en Revistas Arbitradas No Indexadas"
-      
-        case value:
-          // estado=Aceptado en Vías de Publicación
-          return "Articulos Aceptados en Vias de Publicacion"
-      
-        default:
-          return "Publicaciones en Revistas Arbitradas"
-      } */
-      return "Publicaciones en Revistas Arbitradas"
+      // TODO: No se sabe exactamente que condicion hace falta para diferenciarlo de la anterior.
+      // En el codigo viejo al parecer solo se usa en 'sinai/evaluacion_gid.php', pero no hay
+      // una clara distincion. No se puede agregar la verificacion del pais
+      // } else if () {
+      //   // indice is not null & estado=publicado & indice!=todos los anteriores
+      //   return "Publicaciones en Revistas Indexadas en Otros Indices Internacional"
+    
+      } else if (!act_kind.indice && act_kind.estado === "Publicado") {
+        return "Publicaciones en Revistas Arbitradas No Indexadas"
+    
+      } else if (act_kind.estado === "Aceptado en Vias de Publicacion") {
+        return "Articulos Aceptados en Vías de Publicación"
+    
+      } else {
+        return "Publicaciones en Revistas Arbitradas"
+      }
   
     case "capitulos_libros":
       return "Publicaciones de Capotulos de Libros";
@@ -56,7 +60,21 @@ export const match_kind_to_title: (kind: string) => string = (kind: string) => {
       return "Composiciones Solicitadas por Orquestas Sinfonicas o Agrupaciones Reconocidas";
   
     case "eventos":
-      return "EVENTOS";
+      if (act_kind.modalidad === "Invitada" && act_kind.pais === "Venezuela") {
+        return "Asistencia a Eventos Nacionales"
+        
+      } else if (act_kind.modalidad === "Invitada" && act_kind.pais !== "Venezuela") {
+        return "Asistencia a Eventos Internacionales"
+
+      } else if (act_kind.pais === "Venezuela") {
+        return "Eventos en Venezuela"
+        
+      } else if (act_kind.pais !== "Venezuela") {
+        return "Eventos en el Exterior"
+
+      } else {
+        return "Eventos";
+      }
   
     case "exposiciones":
       return "Seleccion en Exposiciones, Bienales, Salones o Concursos Arbitrados";
@@ -68,30 +86,33 @@ export const match_kind_to_title: (kind: string) => string = (kind: string) => {
       return "Informes Tecnicos";
   
     case "libros":
-      /* switch (key) {
-        case value:
-          return "Libro Nacional"
-      
-        case value:
-          return "Libro Internacional"
-      
-        case value:
-          return "Publicaciones de Libros"
-      
-        default:
-          return "LIBROS"
-      } */
-      return "LIBROS"
+      if (act_kind.pais === "Venezuela") {
+        return "Libro Nacional"
+
+      } else if (act_kind.pais !== "Venezuela") {
+        return "Libro Internacional"
+
+      } else {
+        return "Publicaciones de Libros"
+      }
   
     case "memorias":
-      return "MEMORIAS";
+      return "Memorias *Arbitradas* de Congresos";
   
     case "partituras":
       return "Partituras, Video o CD's Publicados en Editoriales Reconocidas";
   
     case "patentes":
-      return "PATENTES";
-  
+      if (act_kind.pais === "Venezuela") {
+        return "Patentes Nacional"
+
+      } else if (act_kind.pais !== "Venezuela") {
+        return "Patentes Internacional"
+
+      } else {
+        return "Patentes"
+      }
+
     case "premios":
       return "Premios";
   
@@ -99,10 +120,43 @@ export const match_kind_to_title: (kind: string) => string = (kind: string) => {
       return "Trabajos Reconocidos o Premiados En Bienales, Salones, Concursos o Exposiciones";
   
     case "proyectos_grado":
-      return "PROYECTOS_GRADO";
+      if (act_kind.nivel_academico === "Doctorado") {
+        return "Tutoria de Tesis Doctorales"
+
+      } else if (act_kind.nivel_academico === "Maestria") {
+        return "Tutoria de Trabajos de Grado (Maestrias)"
+
+      } else if (act_kind.nivel_academico === "Especializacion") {
+        return "Tutoria de Proyectos de Grado (Especializaciones)"
+
+      } else if (act_kind.nivel_academico === "Postgrado") {
+        return "Proyectos de Grado (Postgrados)"
+
+      } else if (act_kind.nivel_academico === "Licencitura") {
+        return "Tutoria de Proyectos de Grado (Licencituras)"
+
+      } else if (act_kind.nivel_academico === "Ingenieria") {
+        return "Tutoria de Proyectos de Grado (Ingenierias)"
+
+      } else if (act_kind.nivel_academico === "Pasantia_Larga") {
+        return "Proyectos de Grado (Pasantias Largas)"
+
+      } else {
+        return "Proyectos de Grado Dirigidos";
+      }
   
     case "proyectos_investigacion":
-      return "PROYECTOS_INVESTIGACION";
+
+      const start = new Date(act_kind.fecha_inicio);
+      start.setMonth(start.getMonth() + act_kind.meses_duracion);
+      const is_active = start >= new Date();
+
+      if (is_active) {
+        return "Proyectos de IYD (Vigentes)";
+        
+      } else {
+        return "Proyectos de IYD";
+      }
   
     case "recitales":
       return "Recitales o Conciertos Arbitrados";
