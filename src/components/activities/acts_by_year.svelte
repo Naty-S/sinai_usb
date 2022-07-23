8d7cbd4832b7d5da8a931ce2df0329463d9c8937
“Naty-S” <na7a5cha010@gmail.com>
2022-07-22 22:14:39 -0400

    split into components activities page

---
+++ src/components/activities/acts_by_year.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import type { Actividad, YearActivities } from "$types/db/actividades";
+  import { format_date, match_kind_to_title } from "$utils/index";
+  import KindInfo from "./kind_info.svelte";
+  
+  export let activities: YearActivities;
+</script>
+
+<h2 class="uk-text-center">
+  Actividades Correspondientes al año {activities.year}
+</h2>
+
+{#each Object.entries(activities.acts) as [kind, acts]}
+<!-- Display activities kind -->
+  <h3>{match_kind_to_title(kind)}</h3>
+
+  <ol class="ui items">
+    {#each acts as act}
+      <div class="item">
+        <li>
+          <div class="content">
+            <strong>autor1; autor2</strong>.
+            "{act.titulo}".
+            <KindInfo activity={act.kind_info} kind={act.kind_name} />
+            <!-- Show group if kind: 
+              articulos_revistas, 
+              capitulos_libros, 
+              eventos, 
+              informes_tecnicos,
+              libros, 
+              memorias,
+              proyectos_grado,
+              proyectos_investigacion
+            -->
+            <span class="uk-text-emphasis">Realizada en el Grupo</span>: act.group.name.
+            
+            <i>{act.observaciones ? "Observaciones: " + act.observaciones : ''}</i>
+    
+            <!-- Shown when rol is: ... -->
+            <span class="uk-text-emphasis">Creada por</span>: {act.creada_por} el {format_date(act.fecha_creacion)}
+            <span class="uk-text-emphasis">Ultima modificacion</span>: act.modificacion
+            {act.validado_por ? "Validada por: " + act.validado_por : ''}
+    
+            <!-- Shown when rol is: ... -->
+            <a href="/api/actividades/[id]/modificar">[Modificar]</a>
+            <a href="/api/actividades/[id]/validar">[Validar]</a>
+            <a href="/api/actividades/[id]/eliminar">[Eliminar]</a>
+          </div>
+        </li>
+      </div>
+    {/each}
+  </ol>
+{/each}
