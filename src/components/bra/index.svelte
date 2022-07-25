50edd889b7c2ae5fe3a2438a7f77246332954e4b
“Naty-S” <na7a5cha010@gmail.com>
2022-07-25 18:36:19 -0400

    + bra view

---
+++ src/components/bra/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import ActsByYear from "$components/activities/acts_by_year.svelte";
+  import BraHeader from "$components/bra/header.svelte";
+
+  export let activities = {year: '1', acts: {}}
+</script>
+
+<BraHeader />
+<ActsByYear {activities} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,9 @@
 <script>
+  import Bra from "$components/bra/index.svelte";
+
 </script>
 
 <main>
   <a class="uk-button" href="/actividades/profesor">Get activities Prof</a>
+  <Bra />
 </main>
