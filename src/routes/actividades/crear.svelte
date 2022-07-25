5417307f153428b3683fe7f664ef1a6f46189770
“Naty-S” <na7a5cha010@gmail.com>
2022-07-25 12:45:16 -0400

    + create & back buttons

--- src/routes/actividades/crear.svelte
+++ src/routes/actividades/crear.svelte
@@ -2,6 +2,7 @@
   import ArticuloRevista from "$components/activities/forms/articulo_revista.svelte";
 	import CapituloLibro from "$components/activities/forms/capitulo_libro.svelte";
 	import Composicion from "$components/activities/forms/composicion.svelte";
+  import CreateBackButtons from "$components/activities/forms/create_back_buttons.svelte";
   import Evento from "$components/activities/forms/evento.svelte";
   import Exposicion from "$components/activities/forms/exposicion.svelte";
   import Grabacion from "$components/activities/forms/grabacion.svelte";
@@ -15,10 +16,18 @@
   import ProyectoGrado from "$components/activities/forms/proyecto_grado.svelte";
   import ProyectoInvestigacion from "$components/activities/forms/proyecto_investigacion.svelte";
   import Recital from "$components/activities/forms/recital.svelte";
+
+  const articulo_revista = true;
+  const capitulo_libro = true;
 </script>
 
-<ArticuloRevista />
-<CapituloLibro />
+{#if articulo_revista}
+  <ArticuloRevista />
+{:else if capitulo_libro}
+  <CapituloLibro />
+{:else}
+  ERROR, this kind of activity can't be created
+{/if}
 <Composicion />
 <Evento />
 <Exposicion />
@@ -33,3 +42,5 @@
 <ProyectoGrado />
 <ProyectoInvestigacion />
 <Recital />
+
+<CreateBackButtons />
