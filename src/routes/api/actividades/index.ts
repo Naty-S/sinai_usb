4faab0530501b72160b43f4ed75535a34ffd29bf
“Naty-S” <na7a5cha010@gmail.com>
2022-07-25 14:23:31 -0400

    + basic idea for redirect depending on role

--- src/routes/api/actividades/index.ts
+++ src/routes/api/actividades/index.ts
@@ -1,15 +1,35 @@
+/* TODO: Redirecciona a la pagina correspondiente dependiendo de si el rol es prof, coord, dpto, etc ... */
 import type { RequestHandler } from "@sveltejs/kit";
-import PrismaClient from "$lib/prisma";
 
-const prisma = new PrismaClient();
-
-export const get: RequestHandler = async ({request}) => {
+export const get: RequestHandler = async ({ request, params }) => {
+  console.log(params)
+  let status = 303;
   let body = {};
-  let status = 500;
+  let headers = { location: '/' };
+  let rol = "";
 
+  if (rol === "profesor") {
+    headers = { location: "/actividades/profesor" }
+
+  } else if (rol === "coordinador") {
+    headers = { location: "/actividades/coordinador" }
+    
+  } else if (rol === "decano") {
+    headers = { location: "/actividades/decano" }
+    
+  } else if (rol === "jefe_departamento") {
+    headers = { location: "/actividades/jefe_departamento" }
+    
+  } else if (rol === "representante_departamento") {
+    headers = { location: "/actividades/representante_departamento" }
+    
+  } else {
+    status = 500
+  };
 
   return {
     status,
+    headers,
     body
   }
 }
