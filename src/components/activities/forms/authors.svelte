156e4fc1cc55434cb8fda9f4ab8fd58b3a70229f
“Naty-S” <na7a5cha010@gmail.com>
2022-07-24 22:36:19 -0400

    authors enhance display

--- src/components/activities/forms/authors.svelte
+++ src/components/activities/forms/authors.svelte
@@ -1,17 +1,33 @@
 <script>
   // export let autores = [];
 </script>
+<!-- TODO: show text input when click 'agregar' -->
+<div>
+  <label class="required field">Autores</label>
+</div>
 
 <div class="four inline fields">
-  <label class="required" for="">Autores</label>
+  <label for="">Externos</label>
 
-  <div class="five wide field">
-    <label for="">Externos</label>
+  <div class="eight wide field">
     <input type="text" placeholder="Nombre">
   </div>
 
-  <div class="field">
-    <label for="">USB</label>
+  <div class="two wide field">
+    <label for="">Ponente</label>
+    <input class="ui checkbox" type="checkbox">
+  </div>
+  
+  <button class="ui red button">Elminar</button>
+</div>
+
+<button class="ui blue button">Agregar</button>
+<button class="ui red button">Limpiar</button>
+
+<div class="four inline fields">
+  <label for="">USB</label>
+
+  <div class="eight wide field">
     <!-- TODO: dropdown con los usuarios -->
     <input type="text" placeholder="Nombre">
   </div>
