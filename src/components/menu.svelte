f04d59a06489a268ef388e0663fa9e3d38ebf296
“Naty-S” <na7a5cha010@gmail.com>
2022-07-26 18:26:01 -0400

    + convert to modal #23

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -10,7 +10,25 @@
         <div class="uk-navbar-dropdown">
           <ul class="uk-nav uk-navbar-dropdown-nav">
             <li><a href="#">Resumen</a></li>
-            <li><a href="#">Ingresar</a></li>
+            <!-- TODO: #23 -->
+            <li class="uk-nav-divider"></li>
+            <li class="uk-nav-header">Ingrsar</li>
+            <li><a href="#">Artículos en Revistas</a></li>
+            <li><a href="#">Asistencia a Eventos</a></li>
+            <li><a href="#">Memorias de Congresos</a></li>
+            <li><a href="#">Proyectos de Investigación y Desarrollo</a></li>
+            <li><a href="#">Recitales o Conciertos Arbitrados</a></li>
+            <li><a href="#">Composiciones solicitadas por Orquestas Sinfónicas o Agrupaciones Reconocidas</a></li>
+            <li><a href="#">Grabaciones Sonoras Evaluadas por Arbitros</a></li>
+            <li><a href="#">Partituras, Videos o CDs publicados en editoriales reconocidas</a></li>
+            <li><a href="#">Selección en Exposiciones, Bienales, Salones o Concursos Arbitrados</a></li>
+            <li><a href="#">Trabajos Reconocidos o Premiados en Bienales, Salones, Concursos o Exposiciones</a></li>
+            <li><a href="#">Libros Publicados</a></li>
+            <li><a href="#">Capítulos de Libros</a></li>
+            <li><a href="#">Informes Técnicos</a></li>
+            <li><a href="#">Premios</a></li>
+            <li><a href="#">Patentes</a></li>
+            <li><a href="#">Proyectos de Grado Dirigidos</a></li>
           </ul>
         </div>
       </li>
@@ -43,7 +61,7 @@
             <li><a href="#">S2</a></li>
             <li><a href="#">GID</a></li>
             <li class="uk-nav-divider"></li>
-            <li class="uk-nav-header">Grupos</li>
+            <li class="uk-nav-header">Mis Grupos</li>
             <li><a href="#">GID</a></li>
           </ul>
         </div>
