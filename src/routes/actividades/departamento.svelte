1fb052a6f70c269bf8b811cac8d2002995d98db1
“Naty-S” <na7a5cha010@gmail.com>
2022-07-26 19:35:13 -0400

    + departament view

--- src/routes/actividades/departamento.svelte
+++ src/routes/actividades/departamento.svelte
@@ -2,3 +2,114 @@
   @component
   Shows the departaments activities resume.
  -->
+
+<h2>El departamento de (dpto name) tiene en el sistema</h2>
+
+<!--  -->
+<table class="ui celled table uk-table-small">
+  <thead>
+    <tr class="center aligned">
+      <th class="uk-table-expand">Actividad</th>
+      <th>Anios</th>
+      <th><i>Total</i></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="uk-table-expand">
+        activity kind title
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned">row activities count</td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="uk-text-center">
+ <a href="">Vista BRA Departamental</a>
+</div>
+
+<div class="uk-text-center">
+  Numero total de profesores de su departamento resgistrados en el sistema: (#)
+</div>
+
+<div class="uk-text-center">
+  Nota: La suma de las actividades de los profesores no es igual al total del departamento,
+  pues pueden tener varios autores.
+</div>
+<!--  -->
+
+<h2>Profesores con Actividades (#)</h2>
+<div class="uk-text-center">
+  Nota: A continuacion se muestran los totales de las actividades ingresadas en el sistema
+  de cada profesor. Si desea ver mas detalles de las actividades de cada profesor haga
+  click en su nombre.
+</div>
+
+<table class="ui celled table uk-table-small">
+  <thead>
+    <tr class="center aligned">
+      <th class="uk-table-expand">Profesor</th>
+      <th>Eventos en el Exterior</th>
+      <th>Eventos Nacionales</th>
+      <th>Proyectos</th>
+      <th>Memorias</th>
+      <th>Informes Tecnicos</th>
+      <th>Articulos</th>
+      <th>Libros</th>
+      <th>Capitulos</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="uk-table-expand">
+        <a href="#">prof name</a>
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+      <td class="center aligned uk-table-expand">
+        activity kind count
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="ui divider" />
+
+<h2>Profesores sin Actividades (#)</h2>
+
+<table class="ui celled table uk-table-small">
+  <thead>
+    <tr class="center aligned">
+      <th class="uk-table-expand">Profesor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="uk-table-expand">
+        <a href="#">prof name</a>
+      </td>
+  </tbody>
+</table>
+
