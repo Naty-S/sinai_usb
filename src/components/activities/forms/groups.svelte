e129a1b7d6b4a0629577ed8db3c9cb31ea64bd1a
“Naty-S” <na7a5cha010@gmail.com>
2022-08-23 12:20:35 -0400

    + groups dropdown

--- src/components/activities/forms/groups.svelte
+++ src/components/activities/forms/groups.svelte
@@ -1,9 +1,51 @@
-<script>
-  export let grupo = '';
+<script lang="ts">
+  import type { activity_form_ctx, kinds } from "$types/forms";
+  import type { Group } from "$interfaces/groups";
+
+  import { getContext, onMount } from "svelte";
+  import { key } from "svelte-forms-lib";
+  import { page } from "$app/stores";
+
+  let groups: Group[] = [];
+
+  const param = $page.params.activity;
+  const kind = param as kinds;
+  const { form, errors, handleChange }: activity_form_ctx<typeof kind> = getContext(key);
+
+  onMount(async () => {
+    const res = await fetch(`/api/groups`);
+    
+    try {      
+      if (res.ok) {
+        groups = await res.clone().json();
+      };
+    } catch (error) {      
+      throw error;
+    };
+  });
 </script>
 
-<div class="field">
-  <label for="">Esta Actividad fue realizada en el Grupo</label>
-  <!-- TODO: dropdown con todos los grupos -->
-  <input type="text" bind:value={grupo}>
-</div>
+{#if $form.actividades_grupos}
+  <div class="field">
+    <label for="actividades_grupos[0]">
+      Esta Actividad fue realizada en el Grupo
+    </label>
+    <select
+      name="actividades_grupos[0]"
+      class="ui fluid selection dropdown"
+      bind:value={$form.actividades_grupos[0]}
+      on:change={handleChange}
+    >
+      {#each groups as g}
+        <option value={g.id}>{g.id} - {g.nombre}</option>
+      {/each}
+    </select>
+    {#if $errors.actividades_grupos}
+      <div class="ui mini error message">
+        {$errors.actividades_grupos[0]}
+      </div>
+    {/if}
+  </div>
+{:else}
+  no
+{/if}
