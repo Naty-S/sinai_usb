4efa7cdd9393ccac02726db0b979caf768fed5d1
“Naty-S” <na7a5cha010@gmail.com>
2022-08-23 12:23:44 -0400

    + country & states dropdown

--- src/components/activities/forms/country_states.svelte
+++ src/components/activities/forms/country_states.svelte
@@ -1,25 +1,57 @@
 <script lang="ts">
-  import { getCountries, getStatesByCode }  from 'cs-list';
+  import type { activity_form_ctx, kinds } from "$types/forms";
 
+  import { getContext } from "svelte";
+  import { key } from "svelte-forms-lib";
+  import { page } from "$app/stores";
+  import { getCountries, getStatesByName } from 'cs-list';
+
+  const param = $page.params.activity;
+  const kind = param as kinds;
+  const { form, errors, handleChange }: activity_form_ctx<typeof kind> = getContext(key);
   const countries = getCountries();
-  let select_country = "VE";
+  
+  $: states = getStatesByName($form.recital.pais);
 </script>
 
 <div class="required field">
-  <label for="pais">Pais</label>
-  <select id="pais" class="ui fluid selection dropdown" bind:value={select_country}>
+  <label for="recital.pais">Pais</label>
+  <select
+    name="recital.pais"
+    class="ui fluid selection dropdown"
+    bind:value={$form.recital.pais}
+    on:change={handleChange}
+  >
     {#each countries as country}
-      <option value={country.code}>{country.name}</option>
+      <option value={country.name}>{country.name}</option>
     {/each}
   </select>
-  </div>
+</div>
 
-<div class="required field">
-  <label for="ciudad">Ciudad</label>
-  <select id="ciudad" class="ui fluid selection dropdown">
-    <!-- TODO: It can be empty states, ex 'AX', display text input if so -->
-    {#each getStatesByCode(select_country) as state}
-      <option value={state.name}>{state.name}</option>
-    {/each}
-  </select>
+<div class="required field" class:error={$errors.recital.ciudad}>
+  <label for="recital.ciudad">Ciudad</label>
+  {#if states}
+    <select
+      name="recital.ciudad"
+      class="ui fluid selection dropdown"
+      bind:value={$form.recital.ciudad}
+      on:change={handleChange}
+    >
+      {#each states as state}
+        <option value={state.name}>{state.name}</option>
+      {/each}
+    </select>
+  {:else}
+    <input
+      type="text"
+      name="recital.ciudad"
+      bind:value={$form.recital.ciudad}
+      on:change={handleChange}
+    >
+  {/if}
+  {#if $errors.recital.ciudad}
+    <div class="ui five wide field mini error message">
+      Requerido
+    </div>
+  {/if}
 </div>
