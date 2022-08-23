564130b71e53b9a3171baedb7c73d8996c0e5d02
“Naty-S” <na7a5cha010@gmail.com>
2022-08-23 12:23:25 -0400

    + recital form

--- src/components/activities/forms/recital.svelte
+++ src/components/activities/forms/recital.svelte
@@ -1,50 +1,74 @@
-<script>
+<script lang="ts">
+  import { setContext } from "svelte";
+  import { createForm, key } from "svelte-forms-lib";
+
+  import { init  } from "$lib/shared/forms/init";
+  import { validationSchema  } from "$lib/shared/forms/validation";
+  import { submit  } from "$lib/shared/forms/submit";
+
+  import ActionsButtons from "./actions_buttons.svelte";
   import Authors from "./authors.svelte";
   import CountryStates from "./country_states.svelte";
+  import Observaciones from './observaciones.svelte';
+  import Input from "./input.svelte";
 
-  const act_data =
-  { titulo: undefined
-  , 
-  }
+  const initialValues = init("recital");
+  const onSubmit = submit("recital");
+  const formProps = { initialValues, onSubmit, validationSchema };
+  const { form, errors, handleChange, handleSubmit, handleReset } = createForm(formProps);
+
+  setContext(key, {
+    form, errors, handleChange
+  });
 </script>
 
 <h2 class="uk-text-center">
   RECITALES O CONCIERTOS ARBITRADOS
 </h2>
 
-<div class="ui large form">
-
-  <div class="required field">
-    <label for="">Título de la Obra</label>
-    <input type="text" bind:value={act_data.titulo}>
-  </div>
-
-  <div class="required field">
-    <label for="">Nombre del Evento</label>
-    <input type="text" bind:value={act_data.titulo}>
-  </div>
-
+<form class="ui large form" on:submit|preventDefault={handleSubmit}>
+  <Input
+    label="Título de la Obra"
+    name="actividad.titulo"
+    bind:value={$form.actividad.titulo}
+    error={$errors.actividad.titulo}
+    class="required field"
+  />
+  <Input
+    label="Nombre del Evento"
+    name="recital.nombre_evento"
+    bind:value={$form.recital.nombre_evento}
+    error={$errors.recital.nombre_evento}
+    class="required field"
+  />
   <div class="three inline fields">
     <CountryStates />
-    
-    <div class="required field">
-      <label for="">Fecha del Evento</label>
-      <input type="date" name="" id="">
-    </div>
+    <Input
+      type="date"
+      label="Fecha del Evento"
+      name="recital.fecha_evento"
+      bind:value={$form.recital.fecha_evento}
+      error={$errors.recital.fecha_evento}
+      class="required field"
+    />
   </div>
-
   <div class="two inline fields">
-    <div class="eight wide required field">
-      <label for="">Jurado o Árbitro</label>
-      <input type="text" bind:value={act_data.titulo}>
-    </div>
-
-    <div class="eight wide field">
-      <label for="">Financiado Por</label>
-      <input type="text" bind:value={act_data.titulo}>
-    </div>
+    <Input
+      label="Jurado o Árbitro"
+      name="recital.jurado"
+      bind:value={$form.recital.jurado}
+      error={$errors.recital.jurado}
+      class="eight wide required field"
+    />
+    <Input
+      label="Financiado Por"
+      name="recital.financiado_por"
+      bind:value={$form.recital.financiado_por}
+      error={$errors.recital.financiado_por}
+      class="eight wide field"
+    />
   </div>
-
   <Authors />
-
-</div>
+  <Observaciones />
+  <ActionsButtons />
+</form>
