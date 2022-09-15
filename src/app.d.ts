b1b2f97520193580e8abd46dacc00d4aebcd7ad2
“Naty-S” <na7a5cha010@gmail.com>
2022-09-15 14:49:35 -0400

    Use cookies to persist session data Fixes #56

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,19 @@
 /// <reference types="@sveltejs/kit" />
+import { User } from "$interfaces/auth";
+
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
-}
+declare global {
+	declare namespace App {
+		interface Locals {
+			user: User;
+		}
+		// interface Platform {}
+		interface Session {
+			user: User;
+		}
+		// interface Stuff {}
+	};
+};
