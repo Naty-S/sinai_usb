86f91eda455b366baf8cc3320e27cbb771cd7833
“Naty-S” <na7a5cha010@gmail.com>
2022-09-16 22:00:28 -0400

    fix login api

--- src/routes/api/auth/login/[user].ts
+++ src/routes/api/auth/login/[user].ts
@@ -28,7 +28,13 @@ export const post: RequestHandler = async function({ request, params }) {
           include: {
             ppi: true,
             grupos_investigacion: true,
-            historico_grupos: true,
+            historico_grupos: {
+              select: {
+                Grupo: true,
+                inicio: true,
+                fin: true,
+              }
+            },
             coordinacion: true,
             division: true,
             jefe_departamentos: true,
@@ -40,20 +46,19 @@ export const post: RequestHandler = async function({ request, params }) {
 
     const professor = _user.profesor;
     
-    let user: User = {
-      email: _user.login,
-      is_dean: !_user.administrador
+    const user: User = {
+      email: _user.login
     };
     let jwt = '';
 
     if (professor) {
       
       const coord_chief = professor.coordinacion ?
-        { coord_id: professor.coordinacion.id, coord_name: professor.coordinacion.nombre }
+        { id: professor.coordinacion.id, name: professor.coordinacion.nombre }
         : undefined
       ;
       const division_chief = professor.division ?
-        { division_id: professor.division.id, division_name: professor.division.nombre }
+        { id: professor.division.id, name: professor.division.nombre }
         : undefined
       ;
       const professor_department = await prisma.departamento.findUniqueOrThrow({
@@ -67,45 +72,53 @@ export const post: RequestHandler = async function({ request, params }) {
               id: true,
               nombre: true
             }
+          },
+          Division: {
+            select: {
+              id: true,
+              nombre: true
+            }
           }
         }
       });
 
-      user = {
-        email: professor.correo
-        , is_dean: false
-        , professor: {
-            id_card: professor.cedula
-          , name1: professor.nombre1
-          , name2: professor.nombre2
-          , surname1: professor.apellido1
-          , surname2: professor.apellido2
-          , category: professor.categoria
-          , dedication: professor.dedicacion
-          , department: {
-              id: professor.departamento
-            , name: professor_department.nombre
-            , coordination: professor_department.Coordinacion
-          }
-          , groups: ["g1","g2"]
-          , diploma: professor.diploma_tipo
-          , diploma_university: professor.diploma_universidad
-          , ppi_number: professor.ppi[0].numero
-          , ppi_level: professor.ppi[0].nivel
-          , profile: professor.perfil
-          , page: professor.url
-          , research_lines: professor.lineas_investigacion
-          , is_dep_chief: professor.jefe_departamentos.length > 0
-          , is_dep_representative: professor.representante_departamentos.length > 0
-          , coord_chief
-          , division_chief
+      user.professor = {
+          id_card: professor.cedula
+        , name1: professor.nombre1
+        , name2: professor.nombre2
+        , surname1: professor.apellido1
+        , surname2: professor.apellido2
+        , category: professor.categoria
+        , dedication: professor.dedicacion
+        , department: {
+            id: professor.departamento
+          , name: professor_department.nombre
+          , coordination: professor_department.Coordinacion
+          , division: professor_department.Division
         }
+        , groups: {
+            grupos_investigacion: professor.grupos_investigacion
+          , historico_grupos: professor.historico_grupos
+        }
+        , diploma: professor.diploma_tipo
+        , diploma_university: professor.diploma_universidad
+        , ppi_number: professor.ppi[0].numero
+        , ppi_level: professor.ppi[0].nivel
+        , profile: professor.perfil
+        , page: professor.url
+        , research_lines: professor.lineas_investigacion
+        , is_dep_chief: professor.jefe_departamentos.length > 0
+        , is_dep_representative: professor.representante_departamentos.length > 0
+        , coord_chief
+        , division_chief
       };
 
       jwt = Buffer.from(JSON.stringify(professor)).toString("base64");
       
     } else if (_user.administrador) {
       console.log("Admin, todas las actividades")
+
+      user.dean = _user.administrador.nombre;
       jwt = Buffer.from(JSON.stringify(_user.administrador)).toString("base64");
 
     } else {
@@ -113,10 +126,10 @@ export const post: RequestHandler = async function({ request, params }) {
     };
 
     status = 200;
-    // TODO: Path -> Path=/sinai
     // cookie expires in 24 hours = 86400 seg
+    // must specify Domain so the cookie is propagated to subdomains
     headers = {
-      "set-cookie": `jwt=${jwt}; Path=/; HttpOnly; Max-Age=86400`
+      "set-cookie": `jwt=${jwt}; Path=/sinai; HttpOnly; Max-Age=86400; Domain=/sinai;`
     };
     body = user;
 
