b69fc92f3f2a7648a1f6a2c9af40de4ee574b2c8
“Naty-S” <na7a5cha010@gmail.com>
2022-09-17 20:58:26 -0400

    + pending prof validations notification

---
+++ src/components/notifications.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { session } from "$app/stores";
+
+  $: user = $session.user;
+</script>
+
+{#if user?.pending_professors && (user?.dean || user?.professor?.coord_chief)}
+  <div class="ui center aligned floating icon mini orange message">
+    <!-- <a class="uk-alert-close" uk-close></a> -->
+    <i class="exclamation icon"></i>
+    <div class="content">
+      <div class="header">
+        Ha profesores pendientes por validar
+      </div>
+      <a href="/sinai/validaciones/nuevos_profesores">
+        Ver Profesores
+      </a>
+    </div>
+  </div>
+{/if}
