6001cb22364ee71d3bcd5ea685bf2dd6b2f88ac6
“Naty-S” <na7a5cha010@gmail.com>
2022-09-17 21:00:40 -0400

    + notifications & totop

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,24 @@
 <!-- Apply code to all `routes` files -->
-<script>
+<script lang="ts">
 	import { navigating } from "$app/stores";
 
-  import Menu from "$components/menu/menu.svelte";
+  import NavMenu from "$components/nav_menu/nav_menu.svelte";
   import Loader from "$components/loader.svelte";
+  import Notifications from "$components/notifications.svelte";
 </script>
 
-<Menu />
+<NavMenu />
+<Notifications />
 
-<main>
+<main class="uk-margin">
   <div class="ui container">
     {#if $navigating}
       <Loader />
     {:else}
       <slot />
+      <a href="#sinai_navabar" uk-totop uk-scroll class="uk-float-right">
+        Top
+      </a>
       <div class="ui divider" />
     {/if}
   </div>
