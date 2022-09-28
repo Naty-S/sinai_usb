a078658d511052fafa32b5a7d8469bf2c4389b38
“Naty-S” <na7a5cha010@gmail.com>
2022-09-28 18:39:33 -0400

    fix: sticky menu

--- src/components/nav_menu/nav_menu.svelte
+++ src/components/nav_menu/nav_menu.svelte
@@ -14,7 +14,7 @@
     let rank = '';
 
     if (professor) {
-      name = `Prof. ${professor.profile}. `;
+      name = `Prof. ${professor.surname1}, ${professor.name1}. `;
 
       if (professor.is_dep_chief) {
         rank = `Jefe del Dpto. de ${professor.department.name}`;
@@ -44,24 +44,25 @@
   let show_create = false;
 </script>
 
-<nav
-  id="sinai_navabar"
-  class="uk-navbar uk-navbar-container"
-  uk-navbar="mode:click; offset: -10; delay-hide: 200"
-  uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky"
->
-  <div id="nav_container" class="uk-navbar-center">
-    <div class="">SINAI - DID</div>
-    {#if $session.user}
-      <div class="">{username($session.user)}</div>
-
-      <MenuDropdown show_create={() => show_create = true} />
-      
-    {:else}
-      <a id="register" href="/sinai/registro" class="">Registro</a>
-    {/if}
-  </div>
-</nav>
+<div class="uk-width-1-1 uk-position-fixed" style="top: 0">
+  <nav
+    id="sinai_navabar"
+    class="uk-navbar uk-navbar-container"
+    uk-navbar="mode:click; offset: -10;"
+  >
+    <div id="nav_container" class="uk-navbar-center">
+      <div class="">SINAI - DID</div>
+      {#if $session.user}
+        <div class="">{username($session.user)}</div>
+  
+        <MenuDropdown show_create={() => show_create = true} />
+        
+      {:else}
+        <a id="register" href="/sinai/registro" class="">Registro</a>
+      {/if}
+    </div>
+  </nav>
+</div>
 
 {#if show_create}
   <ActivitiesModal show={show_create} close={() => show_create = false} />
