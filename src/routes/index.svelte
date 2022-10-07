e086b9135e2349843993d4a7fdf90b556c8bc024
“Naty-S” <na7a5cha010@gmail.com>
2022-10-06 23:18:10 -0400

    fix redirects

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts" context="module">
+  import { redirect } from "$lib/shared/session";
+  
+  export const load = redirect;
+</script>
--- src/routes/sinai/index.svelte
+++ src/routes/sinai/index.svelte
@@ -1,28 +1,9 @@
-<!-- 
+<!--
   @component
-  Main page ***www.sinai.did.usb.ve***
-  you can use markdown!!
- -->
+  Redirects according if the user its logged in or not
+-->
 <script lang="ts" context="module">
-	import type { Load } from "@sveltejs/kit";
-
-  export const load: Load = async function ({ session }) {
-
-    let redirect = "/sinai/login";
-
-    if (session.user?.professor) {
-      redirect = `/sinai/actividades/profesor/${session.user.email}`;
-    } else if (session.user?.dean) {
-      redirect = "/sinai/actividades";
-    };
-
-    return {
-      status: 302,
-      redirect
-    }
-  }
+  import { redirect } from "$lib/shared/session";
+  
+  export const load = redirect;
 </script>
-
-<svelte:head>
-	<title>SINAI</title>
-</svelte:head>
