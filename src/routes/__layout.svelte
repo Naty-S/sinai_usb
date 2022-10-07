b82cb91ddb757c373f6893a46a66b73bf570f775
“Naty-S” <na7a5cha010@gmail.com>
2022-10-06 23:33:57 -0400

    fix layout conditions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,37 +1,54 @@
 <!-- Apply code to all `routes` files -->
 <script lang="ts">
+  import { goto } from "$app/navigation";
 	import { navigating, session, page } from "$app/stores";
 
   import Navbar from "$components/navbar/index.svelte";
   import Loader from "$components/loader.svelte";
   import Notifications from "$components/notifications.svelte";
   import ToTop from "$components/to_top.svelte";
+  
   import Login from "./sinai/login.svelte";
+  import Register from "./sinai/registro.svelte";
+  import Err from "./__error.svelte";
 
   $: user = $session.user;
 </script>
 
-<Navbar />
+<svelte:head>
+	<title>SINAI</title>
+</svelte:head>
 
-{#if user?.pending_professors && (user?.dean || user?.professor?.coord_chief)}
-  <Notifications header_msg="Hay profesores pendientes por validar" >
-    <a href="/sinai/validaciones/nuevos_profesores">
-      Ver Profesores
-    </a>
-  </Notifications>
-{/if}
+<Navbar />
 
 <main class="ui container uk-margin">
   {#if $navigating}
     <Loader />
   {:else}
-    {#if ($session.user && $page.url.pathname !== "/sinai/login") || $page.url.pathname === "/sinai/registro"}
+    {#if user && !["login", "registro"].some(path => $page.url.pathname.includes(path))}
+
+      {#if user.pending_professors && (user.dean || user.professor?.coord_chief)}
+        <Notifications header_msg="Hay profesores pendientes por validar" >
+          <a href="/sinai/validaciones/nuevos_profesores">
+            Ver Profesores
+          </a>
+        </Notifications>
+      {/if}
+
       <slot />
-      <ToTop />
-      <div class="ui divider" />
-    {:else}
+
+    {:else if $page.url.pathname === "/sinai/registro"}
+      <Register />
+    {:else if $page.url.pathname === "/sinai/login"}
       <Login />
+    {:else if $page.error}
+      <Err error={$page.error} status={$page.status} />
+    {:else}
+      {goto("/sinai/login")}
     {/if}
+    
+    <ToTop />
+    <div class="ui divider" />
   {/if}
 </main>
   
