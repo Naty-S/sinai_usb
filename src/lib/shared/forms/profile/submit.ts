6060649dd181917e8c4f4464ba56780eab974253
“Naty-S” <na7a5cha010@gmail.com>
2022-10-06 23:36:31 -0400

    use api

--- src/lib/shared/forms/profile/submit.ts
+++ src/lib/shared/forms/profile/submit.ts
@@ -1,22 +1,13 @@
 import { goto } from "$app/navigation";
 
+import * as api from "$lib/api";
+
 
 export const submit = function (email: string, pathname: string) {
   return async function (data: any) {
-    const res = await fetch(`/api/professor/${email}`, {
-      method: "PATCH",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        new: data,
-        pathname
-      })
-    });
+    
+    const res = await api.patch(`/api/professor/${email}`, { new: data, pathname });
 
-    if (res.ok) {
-      goto(res.url);
-    };
+    goto(res.url);
   };
 };
