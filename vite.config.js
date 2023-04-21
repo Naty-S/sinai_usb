80d36df012a29d43f0b06dfbd62ef81905e64ab7
“Naty-S” <na7a5cha010@gmail.com>
2023-04-21 07:37:25 -0400

    Deploy Fixes #79

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,10 @@ const config = {
 			$interfaces: path.resolve('./src/interfaces'),
 			$utils: path.resolve('./src/utils'),
 			$stores: path.resolve('./src/stores'),
+
+			// This is because of some problems with vite it seems
+			// related: https://github.com/prisma/prisma/issues/12504
+			".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
 		},
 	},
 	server: {
