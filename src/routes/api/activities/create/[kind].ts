6e7d441fb8c81e0060297e09899be4ebeb97c4fc
“Naty-S” <na7a5cha010@gmail.com>
2025-04-26 15:24:11 -0400

    fix: patente and premio don't have groups

--- src/routes/api/activities/create/[kind].ts
+++ src/routes/api/activities/create/[kind].ts
@@ -13,9 +13,14 @@ import { ve_date } from "$lib/utils/formatting";
 export const POST: RequestHandler = async ({ request, params }) => {
 
   const _data = await request.json();
+  let actividades_grupos_create = [];
+  if (_data.actividades_grupos.length > 0) {
+    actividades_grupos_create = _data.actividades_grupos.map((g: any) => ({ grupo: Number(g.new) }))
+  };
+
   const data = {
     ..._data.actividad,
-    actividades_grupos: { create: _data.actividades_grupos.map((g: any) => ({ grupo: Number(g.new) })) },
+    actividades_grupos: { create: actividades_grupos_create },
     autores_usb: { create: _data.autores_usb },
     autores_externos: { create: _data.autores_externos },
   };
