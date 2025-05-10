abea6524581246834662078c00ec48b24daf706c
“Naty-S” <na7a5cha010@gmail.com>
2025-05-10 19:44:32 -0400

    fix: init request date today S1 novel

--- src/lib/utils/forms/s1_novel/request/init.ts
+++ src/lib/utils/forms/s1_novel/request/init.ts
@@ -1,10 +1,11 @@
+import { init_date } from "$lib/utils/formatting";
 
 
 export const init = function () { // TODO: create types for inits, so makes easier in back (api). May change in submit
   
   return {
       comentario: ''
-    , fecha_solicitud: ''
+    , fecha_solicitud: init_date()
     , observaciones_evaluador: ''
     , observaciones_profesor: "Sin Observaciones"
     , proyecto: []
