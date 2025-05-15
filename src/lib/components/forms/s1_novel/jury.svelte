b47bc947b251b68b3029737dcb32398d27ad00cb
“Naty-S” <na7a5cha010@gmail.com>
2025-05-15 15:59:41 -0400

    fix: professors name order as s1 novel jury

--- src/lib/components/forms/s1_novel/jury.svelte
+++ src/lib/components/forms/s1_novel/jury.svelte
@@ -121,7 +121,7 @@
           }}
           options={professors.map(p => ({
             val: p.perfil,
-            name: `${p.apellido2 ?? ''} ${p.apellido1}, ${p.nombre2 ?? ''} ${p.nombre1} - ${p.correo}`
+            name: `${p.apellido1} ${p.apellido2 ?? ''}, ${p.nombre1} ${p.nombre2 ?? ''} - ${p.correo}`
           }))}
           class="twelve wide required field"
           error={$errors.jurado_usb[i]?.nombre}
